d1ff85ed58e4c86e2fae9b787f9a72f50136447d
Greg Davill <greg.davill@gmail.com>
2021-10-17 15:14:44 +1030

    Add memory bounds check
    - Improve bootloader_upgrade mode
    - Update LEDs

--- firmware/main.c
+++ firmware/main.c
@@ -24,14 +24,20 @@
 //--------------------------------------------------------------------+
 // MACRO CONSTANT TYPEDEF PROTYPES
 //--------------------------------------------------------------------+
-const uint32_t alt_offsets[] = {
-	0x200000,
-	0x400000,
-	0x800000,
-	0x000000,
+typedef struct{
+	uint32_t address;
+	uint32_t length;
+} memory_offest;
+
+memory_offest const alt_offsets[] = {
+	{.address=0x200000, .length=0x200000}, /* Main Gateware */
+	{.address=0x400000, .length=0x800000}, /* Main Firmawre */
+	{.address=0x800000, .length=0x800000}, /* Extra */
+	{.address=0x000000, .length=0x200000} /* Bootloader */
 };
 
 static int complete_timeout;
+static bool bl_upgrade = false;
 
 /* Blink pattern
  * - 1000 ms : device should reboot
@@ -42,6 +48,7 @@ static int complete_timeout;
 enum
 {
 	BLINK_DFU_IDLE,
+	BLINK_DFU_IDLE_BOOTLOADER,
 	BLINK_DFU_DOWNLOAD,
 	BLINK_DFU_ERROR,
 	BLINK_DFU_SLEEP,
@@ -117,6 +124,7 @@ int main(int i, char **c)
 
 	if(ctrl_scratch_read() == 0){
 		enable_bootloader_alt();
+		bl_upgrade = true;
 	}
 
 	uint8_t last_button = button_in_read();
@@ -143,7 +151,6 @@ int main(int i, char **c)
 					msleep(20);	
 					
 					ctrl_reset_write(1);
-
 				}
 			}
 			else{
@@ -179,10 +186,9 @@ int main(int i, char **c)
 	return 0;
 }
 
-const uint32_t idle_rainbow[] = {0x3ff00032,0x3ff000cb,0x3ff0017e,0x3ff00248,0x3ff00322,0x3f4003ff,0x30f003ff,0x236003ff,0x171003ff,0x0bc003ff,0x027003ff,0x000103ff,0x000367ff,0x00064bff,0x00096bff,0x000cd7ff,0x000fffe1,0x000ffefc,0x000ffe29,0x000ffd61,0x000ffcae,0x000ffc20,0x049ffc00,0x0e8ffc00,0x1a3ffc00,0x26cffc00,0x348ffc00,0x3fff3400,0x3ffba400,0x3ff85c00,0x3ff54400,0x3ff28000,};
+const uint32_t idle_rainbow[] = {0x2aaffc00,0x38000000,0x2aaffc00,0x2aaffc00,0x38000000,0x2aaffc00,0x2aaffc00,0x2aaffc00,0x2aaffc00,0x38000000,0x38000000,0x2aaffc00,0x2aaffc00,0x38000000,0x2aaffc00,0x38000000,0x2aaffc00,0x38000000,0x38000000,0x38000000,0x38000000,0x2aaffc00,0x2aaffc00,0x38000000,0x2aaffc00,0x2aaffc00,0x38000000,0x2aaffc00,0x2aaffc00,0x2aaffc00,0x2aaffc00,0x38000000,};
 const uint16_t sine_falloff_fade[] = {0x3ff,0x3f8,0x3ea,0x3cf,0x3ae,0x387,0x354,0x31b,0x2df,0x29f,0x256,0x210,0x1c5,0x17e,0x138,0x0f4,0x0b8,0x07f,0x04d,0x028,0x00c,0x000,0x000,0x000,0x000,0x000,0x000,0x000,0x000,0x000,0x000,0x000};
 const uint16_t sine_pulse_fade[] = {0x000,0x000,0x000,0x017,0x04d,0x09a,0x0f4,0x15a,0x1c5,0x235,0x29f,0x2fd,0x354,0x39a,0x3cf,0x3f1,0x3ff,0x3f1,0x3cf,0x39a,0x354,0x2fd,0x29f,0x235,0x1c5,0x15a,0x0f4,0x09a,0x04d,0x017,0x000,0x000};
-const uint16_t _gamma[] = {0, 0, 0, 1, 2, 2, 3, 4, 5, 6, 7, 9, 10, 11, 13, 14, 16, 17, 19, 20, 22, 24, 25, 27, 29, 31, 33, 35, 37, 39, 41, 43, 45, 47, 49, 52, 54, 56, 58, 61, 63, 65, 68, 70, 73, 75, 78, 80, 83, 86, 88, 91, 94, 96, 99, 102, 105, 108, 110, 113, 116, 119, 122, 125, 128, 131, 134, 137, 140, 143, 147, 150, 153, 156, 159, 163, 166, 169, 173, 176, 179, 183, 186, 189, 193, 196, 200, 203, 207, 210, 214, 218, 221, 225, 228, 232, 236, 240, 243, 247, 251, 255, 258, 262, 266, 270, 274, 278, 281, 285, 289, 293, 297, 301, 305, 309, 313, 317, 322, 326, 330, 334, 338, 342, 346, 351, 355, 359, 363, 368, 372, 376, 381, 385, 389, 394, 398, 402, 407, 411, 416, 420, 425, 429, 434, 438, 443, 447, 452, 456, 461, 466, 470, 475, 480, 484, 489, 494, 498, 503, 508, 513, 518, 522, 527, 532, 537, 542, 547, 551, 556, 561, 566, 571, 576, 581, 586, 591, 596, 601, 606, 611, 616, 621, 627, 632, 637, 642, 647, 652, 657, 663, 668, 673, 678, 684, 689, 694, 699, 705, 710, 715, 721, 726, 731, 737, 742, 748, 753, 759, 764, 769, 775, 780, 786, 791, 797, 803, 808, 814, 819, 825, 830, 836, 842, 847, 853, 859, 864, 870, 876, 882, 887, 893, 899, 905, 910, 916, 922, 928, 934, 939, 945, 951, 957, 963, 969, 975, 981, 987, 993, 999, 1005, 1010, 1016, 1023};
 
 void led_blinking_task(void)
 {
@@ -190,31 +196,37 @@ void led_blinking_task(void)
 	static int count;
 
 	// Blink every interval ms
-	if ((board_millis() - start_ms) < 50)
+	if ((board_millis() - start_ms) < 40)
 		return; // not enough time
-	start_ms += 50;
+	start_ms += 40;
 	count++;
 
 	volatile uint32_t *p = (uint32_t*)CSR_LEDS_OUT0_ADDR;
 	switch(blink_interval_ms){
 		case BLINK_DFU_IDLE:
 		{
+			if(bl_upgrade){
+				blink_interval_ms = BLINK_DFU_IDLE_BOOTLOADER;
+				break;
+			}
+
 			/* Pick colour from idle_rainbow. Then interger multiply it with a sine_falloff_fade */
-			int colour = ((count + 5) / 32) % 32;
+			int colour = (((count  + 10) / 32)) % 32;
+
 			for(int i = 0; i < 4; i++){
 				int c = (count + i*2);
-				int r = (((idle_rainbow[colour] >> 20) & 0x3FF) * sine_falloff_fade[c % 32]) >> 10;
-				int g = (((idle_rainbow[colour] >> 10) & 0x3FF) * sine_falloff_fade[c % 32]) >> 10;
-				int b = (((idle_rainbow[colour] >> 0 ) & 0x3FF) * sine_falloff_fade[c % 32]) >> 10;
-				p[i] = (r << 20) | (g << 10) | (b);
+				int g = (((idle_rainbow[colour] >> 20) & 0x3FF) * sine_falloff_fade[c % 32]) >> 10;
+				int b = (((idle_rainbow[colour] >> 10) & 0x3FF) * sine_falloff_fade[c % 32]) >> 10;
+				int r = (((idle_rainbow[colour] >> 0 ) & 0x3FF) * sine_falloff_fade[c % 32]) >> 10;
+				p[i] = (g << 20) | (b << 10) | (r);
 			}
 			for(int i = 0; i < 3; i++){
 				int c = (count - (i - 2)*2);
 
-				int r = (((idle_rainbow[colour] >> 20) & 0x3FF) * sine_falloff_fade[c % 32]) >> 10;
-				int g = (((idle_rainbow[colour] >> 10) & 0x3FF) * sine_falloff_fade[c % 32]) >> 10;
-				int b = (((idle_rainbow[colour] >> 0 ) & 0x3FF) * sine_falloff_fade[c % 32]) >> 10;
-				p[4+i] = (r << 20) | (g << 10) | (b);
+				int g = (((idle_rainbow[colour] >> 20) & 0x3FF) * sine_falloff_fade[c % 32]) >> 10;
+				int b = (((idle_rainbow[colour] >> 10) & 0x3FF) * sine_falloff_fade[c % 32]) >> 10;
+				int r = (((idle_rainbow[colour] >> 0 ) & 0x3FF) * sine_falloff_fade[c % 32]) >> 10;
+				p[4+i] = (g << 20) | (b << 10) | (r);
 
 				//p[4 + i] = (uint32_t)sine_falloff_fade[c % 32] << colour; /* BLUE */
 			}
@@ -241,10 +253,26 @@ void led_blinking_task(void)
 		break;
 		
 
-		case BLINK_DFU_ERROR:
+		case BLINK_DFU_IDLE_BOOTLOADER:
 		{
 			for(int i = 0; i < 7; i++){
 				p[i] = (uint32_t)sine_pulse_fade[(count + i*5) % 32] << 0; // RED
+			}
+		}
+		break;
+
+		case BLINK_DFU_ERROR:
+		{
+			for(int i = 0; i < 4; i++){
+				int c = ((count << 2) + i*2);
+				int r = sine_falloff_fade[c % 32];
+				p[i] = (r);
+			}
+			for(int i = 0; i < 3; i++){
+				int c = ((count << 2) - (i - 2)*2);
+
+				int r = sine_falloff_fade[c % 32];
+				p[4+i] = (r);
 			}
 		}
 		break;
@@ -326,12 +354,20 @@ void tud_dfu_download_cb(uint8_t alt, uint16_t block_num, uint8_t const* data, u
 
 	blink_interval_ms = BLINK_DFU_DOWNLOAD;
 
-  	uint32_t flash_address = alt_offsets[alt] + block_num * CFG_TUD_DFU_XFER_BUFSIZE;
+	if((block_num * CFG_TUD_DFU_XFER_BUFSIZE) >= alt_offsets[alt].length){
+		// flashing op for download length error
+		tud_dfu_finish_flashing(DFU_STATUS_ERR_ADDRESS);
 
+		blink_interval_ms = BLINK_DFU_ERROR;
+
+		return;
+	}
+
+  	uint32_t flash_address = alt_offsets[alt].address + block_num * CFG_TUD_DFU_XFER_BUFSIZE;
+
+	/* First block in 64K erase block */
 	if((flash_address & (FLASH_64K_BLOCK_ERASE_SIZE-1)) == 0){
-		//printf("Erasing. flash_address=%08x\n", flash_address);
-
-		/* First block in 64K erase block */
+		
 		spiflash_write_enable();
 		spiflash_sector_erase(flash_address);
 
@@ -363,7 +399,7 @@ void tud_dfu_download_cb(uint8_t alt, uint16_t block_num, uint8_t const* data, u
 void tud_dfu_manifest_cb(uint8_t alt)
 {
   (void) alt;
-  printf("Download completed, enter manifestation\r\n");
+  blink_interval_ms = BLINK_DFU_DOWNLOAD;
 
   // flashing op for manifest is complete without error
   // Application can perform checksum, should it fail, use appropriate status such as errVERIFY.
