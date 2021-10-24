cb170cf9bdccd6516b4d5ff6b18321003d665638
Greg Davill <greg.davill@gmail.com>
2021-10-24 11:21:19 +1030

    Update PID to assigned value 0x5af1

--- firmware/usb_descriptors.c
+++ firmware/usb_descriptors.c
@@ -52,7 +52,7 @@ tusb_desc_device_t const desc_device =
     .bMaxPacketSize0    = CFG_TUD_ENDPOINT0_SIZE,
 
     .idVendor           = 0x1209,
-    .idProduct          = 0x5af0,
+    .idProduct          = 0x5af1,
     .bcdDevice          = 0x0100,
 
     .iManufacturer      = 0x01,
